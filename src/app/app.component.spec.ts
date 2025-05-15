32966066a9d8ffbefc1ca2238ee9ea24e1872305
StaubiiiGIBM <leon.schaub@bbzbl-it.ch>
2025-05-15 15:37:28 +0200

    Fix: Rename Project to correct module number 294
    Renamed missing occurences

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'm-295-absentia-frontend' title`, () => {
+  it(`should have the 'm-294-absentia-frontend' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('m-295-absentia-frontend');
+    expect(app.title).toEqual('m-294-absentia-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, m-295-absentia-frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, m-294-absentia-frontend');
   });
 });
